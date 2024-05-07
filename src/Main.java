d39230e6c17de2b06c91296c23af90515b0e0615
dhiv <vetri93selvi@gmail.com>
2024-05-07 17:21:47 +0530

    task solution

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int a=1,b=2,c=3,d=4;
+        {
+            if((a+b)>(c+d));
+        }
+        System.out.println("a+b is greater than c+d ");
+    }
+}
